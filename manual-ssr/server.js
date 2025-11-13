60643e6cd82f901e43e13af841c6856b8e511af8
ashutosh mishra <mishraashutosh.dev@gmail.com>
2025-11-13 16:58:45 +0530

    Section 32 : Overview of Next.js With the App Router : Expriment: Manual SSR with React DOM + Node.js (yha hamne manually server pr react ko run kia)

---
+++ manual-ssr/server.js
@@ -0,0 +1,89 @@
+const { createServer } = require("http");
+const { readFileSync } = require("fs");
+const { parse } = require("url");
+
+const { renderToString } = require("react-dom/server");
+const React = require("react");
+const pizzas = [
+  {
+    name: "Focaccia",
+    price: 6,
+  },
+  {
+    name: "Pizza Margherita",
+    price: 10,
+  },
+  {
+    name: "Pizza Spinaci",
+    price: 12,
+  },
+  {
+    name: "Pizza Funghi",
+    price: 12,
+  },
+  {
+    name: "Pizza Prosciutto",
+    price: 15,
+  },
+];
+
+function Home() {
+  return (
+    <div>
+      <h1>🍕 Fast React Pizza Co.</h1>
+      <p>This page has been rendered with React on the server 🤯</p>
+
+      <h2>Menu</h2>
+      <ul>
+        {pizzas.map((pizza) => (
+          <MenuItem pizza={pizza} key={pizza.name} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = React.useState(0);
+  return (
+    <div>
+      <button onClick={() => setCount((c) => c + 1)}>+1</button>
+      <span>{count}</span>
+    </div>
+  );
+}
+
+function MenuItem({ pizza }) {
+  return (
+    <li>
+      <h4>
+        {pizza.name} (${pizza.price})
+      </h4>
+      <Counter />
+    </li>
+  );
+}
+
+const server = createServer((req, res) => {
+  const pathName = parse(req.url, true).pathname;
+  const htmlTemplate = readFileSync(`${__dirname}/index.html`, "utf-8");
+
+  if (pathName === "/") {
+    const renderedReact = renderToString(<Home />);
+    const html = htmlTemplate.replace("%%%CONTENT%%%", renderedReact);
+
+    res.writeHead(200, { "content-type": "text/html" });
+
+    // here we are render react component
+    res.end(html);
+
+    //here index.html page run
+    //res.end(htmlTemplate);
+  } else if (pathName === "/test") {
+    res.end("Response from server Test");
+  } else {
+    res.end("The URL can't be Found.");
+  }
+});
+
+server.listen(8000, () => console.log("Listening for request on port 8000"));
