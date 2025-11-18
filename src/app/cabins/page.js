f59670a74fb67bb6db810695b830946adbd5439d
ashutosh mishra <mishraashutosh.dev@gmail.com>
2025-11-18 07:14:37 +0530

    Section 32 : Overview of Next.js With the App Router : Fetching data from fake api for server componet

--- src/app/cabins/page.js
+++ src/app/cabins/page.js
@@ -1,7 +1,18 @@
-export default function Page() {
+export default async function Page() {
+  // ye ek server component hai kyuki next.js me by default jitne bhi component bante hai vo server component hote h
+  // aur yha ham direct  data fetch kar rhe hai bina useEffect ya kisi third party library ke.
+
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  //console.log(data);
   return (
     <div>
       <h1>Cabin Page</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
