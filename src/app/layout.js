2b74d87b71bc2c95d2ed82ace624fc67d89fa0f0
ashutosh mishra <mishraashutosh.dev@gmail.com>
2025-11-18 18:24:49 +0530

    Section 33 : Starting to Build the Wild Osis Website : Loading and optimizing Fonts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,14 @@
 import Logo from "@/app/_component/Logo";
 import Navigation from "@/app/_component/Navigation";
 
+//hamne yha tailwind css apply kiya
 import "@/app/_styles/globals.css";
+
+//hamne yha font style apply kiya
+import { Josefin_Sans } from "next/font/google";
+const josefin = Josefin_Sans({ subsets: ["latin"], display: "swap" }); // yah ek function h esi liye ese yha call kiya gya
+//console.log(josefin);
+//hamne yha metadata apply kiya
 
 export const metadata = {
   //title: "The Wild Osis"
@@ -13,7 +20,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-50 min-h-screen">
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-50 min-h-screen`}>
         <header>
           <Logo />
           <Navigation />
