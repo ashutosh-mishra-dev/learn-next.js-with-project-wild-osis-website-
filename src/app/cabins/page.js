2420fc08a28dead7ddb2a5612564fc7b8d639abe
ashutosh mishra <mishraashutosh.dev@gmail.com>
2025-11-18 15:52:19 +0530

    Section 33 : Starting to Build the Wild Osis  Website : Project organization

--- src/app/cabins/page.js
+++ src/app/cabins/page.js
@@ -1,23 +1,9 @@
-import Counter from "../component/Counter";
+import Counter from "@/app/_component/Counter";
 
 export default async function Page() {
-  // ye ek server component hai kyuki next.js me by default jitne bhi component bante hai vo server component hote h
-  // aur yha ham direct  data fetch kar rhe hai bina useEffect ya kisi third party library ke.
-
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await res.json();
-  //console.log(data);
   return (
     <div>
       <h1>Cabin Page</h1>
-      <ul>
-        {data.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-      {/* ab ham yha server component se client component me data send kar rhe hai
-      props ke through, esi props ko "server - client bridge" bola jata hai  */}
-      <Counter users={data} />
     </div>
   );
 }
