457c82412c7986c4821e205b6a34f94c24629357
ashutosh mishra <mishraashutosh.dev@gmail.com>
2025-11-26 09:56:43 +0530

    Section 35 : Client and Server Interactions : Highlighting Current side Navigation Link

--- src/app/_component/TextExpander.js
+++ src/app/_component/TextExpander.js
@@ -11,18 +11,14 @@ function TextExpander({ children }) {
     : children.split(" ").slice(0, 40).join(" ") + "...";
 
   return (
-    <>
-      <span>
-        {displayText}{" "}
-        <button
-          className="text-primary-700 border-b border-primary-700 leading-3 pb-1 cursor-pointer"
-          onClick={() => setIsExpanded(!isExpanded)}>
-          {isExpanded ? "Show less" : "Show more"}
-        </button>
-      </span>
-      {/* yha Logo component as client instance work karega aur jo header me Logo component aa rha h wo server instance h  */}
-      <Logo />
-    </>
+    <span>
+      {displayText}{" "}
+      <button
+        className="text-primary-700 border-b border-primary-700 leading-3 pb-1 cursor-pointer"
+        onClick={() => setIsExpanded(!isExpanded)}>
+        {isExpanded ? "Show less" : "Show more"}
+      </button>
+    </span>
   );
 }
 
