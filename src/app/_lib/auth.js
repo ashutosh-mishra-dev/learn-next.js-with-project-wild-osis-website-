2dd6c396b4721630e529762176cca113e79b1152
ashutosh mishra <mishraashutosh.dev@gmail.com>
2025-12-02 12:25:28 +0530

    Section 36 : Authentication With NextAuth(AUTH.JS) : Creating new Guest on first signIn and END THIS SECTION

--- src/app/_lib/auth.js
+++ src/app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
 //aap ko 2 example diya h koi bhi use kar sakte ho
 
@@ -39,6 +40,22 @@ const authConfig = {
   callbacks: {
     authorized({ auth, request }) {
       return !!auth?.user;
+    },
+    async signIn({ user, account, profile }) {
+      try {
+        const existingGuest = await getGuest(user.email);
+
+        if (!existingGuest)
+          await createGuest({ email: user.email, fullName: user.name });
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+      return session;
     },
   },
 
--- src/app/account/page.js
+++ src/app/account/page.js
@@ -4,8 +4,11 @@ export const metadata = {
   title: "Account",
 };
 
+//yha hamene Account name diya h function ka but ham yha kuch bhi name de sakte h ex. xxx() ,abc(), page() Sab sahi hai. Next.js ko koi fark nahi padta.
 export default async function Account() {
   const session = await auth();
+  console.log("session : ", session);
+
   const firstName = session.user.name.split(" ").at(0);
   return (
     <div>
