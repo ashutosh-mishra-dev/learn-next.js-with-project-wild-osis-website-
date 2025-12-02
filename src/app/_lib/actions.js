aa0bfaf05d83793dd6c03e885299549d8c9146a1
ashutosh mishra <mishraashutosh.dev@gmail.com>
2025-12-02 17:18:23 +0530

    Section 37 : Mutations With Server Actions + Modern React Hooks : text

--- src/app/_lib/actions.js
+++ src/app/_lib/actions.js
@@ -1,6 +1,14 @@
 "use server";
 
-import { signIn, signOut } from "./auth";
+import { auth, signIn, signOut } from "./auth";
+import { supabase } from "./supabase";
+
+// Ye kya karta hai?
+
+// ✔ NextAuth ka signIn() function call
+// ✔ Google OAuth flow start
+// ✔ user Google page par jata hai
+// ✔ login successful → redirect to /account
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" }); // "google" ye ek providers h man lo google ke alawa aur bhi provider hote jaise github.facebook etc to hame yha loop lagana padta aur jaise login ho jayenge vo hame redirect kar dega account page pr
@@ -10,9 +18,22 @@ export async function signOutAction() {
   await signOut({ redirectTo: "/" }); // sign out hone ke bad root / route pr chala jayega agar root route pr home page h to vo show karega
 }
 
-// Ye kya karta hai?
+export async function updateGuest(formData) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in ");
 
-// ✔ NextAuth ka signIn() function call
-// ✔ Google OAuth flow start
-// ✔ user Google page par jata hai
-// ✔ login successful → redirect to /account
+  const nationalID = formData.get("nationalID");
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+
+  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID))
+    throw new Error("Please Provide a valid nationalId.");
+
+  const updateData = { nationality, countryFlag, nationalID };
+
+  const { data, error } = await supabase
+    .from("guests")
+    .update(updateData)
+    .eq("id", session.user.guestId);
+
+  if (error) throw new Error("Guest could not be updated");
+}
--- src/app/account/profile/page.js
+++ src/app/account/profile/page.js
@@ -1,14 +1,16 @@
 import SelectCountry from "@/app/_component/SelectCountry";
 import UpdateProfileForm from "@/app/_component/UpdateProfileForm";
+import { auth } from "@/app/_lib/auth";
+import { getGuest } from "@/app/_lib/data-service";
 
-export const dynamic = "force-dynamic";
 export const metadata = { title: "Update Profile" };
 
-// CHANGE
+// export const dynamic = "force-dynamic";
 
-const nationality = "India";
+export default async function Page() {
+  const session = await auth();
+  const guest = await getGuest(session.user.email);
 
-export default function Page() {
   return (
     <div>
       <h2 className="font-semibold text-2xl text-accent-400 mb-4">
@@ -20,12 +22,12 @@ export default function Page() {
         faster and smoother. See you soon!
       </p>
 
-      <UpdateProfileForm>
+      <UpdateProfileForm guest={guest}>
         <SelectCountry
           name="nationality"
           id="nationality"
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
-          defaultCountry={nationality}
+          defaultCountry={guest.nationality}
         />
       </UpdateProfileForm>
     </div>
